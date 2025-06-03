3831672aad4596a282802e3906a8419bc9b5a245
R-Szk <trail_stuff_seat@yahoo.co.jp>
2025-06-03 23:15:07 +0900

    指定したカテゴリの月単位の支出額のデータと、グラフを実装

--- src/KakeiboList/categoryKakeiboList.js
+++ src/KakeiboList/categoryKakeiboList.js
@@ -5,18 +5,28 @@ import { useContext } from "react";
 
 import { format, startOfMonth, endOfMonth } from "date-fns";
 
-import { ArcElement, Legend, Tooltip, Chart as chartJs } from "chart.js";
-import { Pie as ChartPie } from "react-chartjs-2";
+// import { ArcElement, Legend, Tooltip, Chart as chartJs } from "chart.js";
+import { Chart as chartJs, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from "chart.js";
+import { Bar, Chart } from "react-chartjs-2";
 import { useEffect, useState } from "react";
 import { supabase } from "../Supabase/supabaseClient";
 import useFetchData from "../Supabase/useFetchData";
 
+chartJs.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Tooltip,
+    Legend
+);
 
 function CategoryKakeiboList() {
     const location = useLocation();
     const navigate = useNavigate();
 
     const { categoryId, displayDay } = location.state || {}; // カテゴリIDと表示日付を取得
+
+    const [monthlyExpensesList, setMonthlyExpensesList] = useState([]);
 
     // 指定したカテゴリ、かつ指定した期間に合致する支出・収入リストを取得する
     const fetchItem = useFetchData(
@@ -32,6 +42,25 @@ function CategoryKakeiboList() {
         ]
     );
 
+    // 指定したカテゴリの月単位の支出・収入リストを取得する
+    useEffect(() => {
+        const fetchItem = async () => {
+            const { data, error } = await supabase.rpc('monthlyexpenseslist', {
+                category_id: categoryId
+            });
+
+            if (error) {
+                console.error('Error fetching item:', error);
+            }
+            else if (data) {
+                // console.log('Fetched item:', data);
+                setMonthlyExpensesList(data);
+            }
+        }
+        fetchItem();
+    }
+    , []);
+
     return (
         // 選択したカテゴリの支出・収入リストを表示
         <div className="bg-gray-100">
@@ -40,6 +69,21 @@ function CategoryKakeiboList() {
                 <div className="max-w-[27rem] mx-auto space-y-2">
                     <div className="p-4 border-b bg-gray-50">
                         <div className="space-y-1">
+                            <Bar
+                                data={{
+                                    labels: monthlyExpensesList.map(item =>
+                                        new Date(item.month).toLocaleDateString('ja-JP', { year: 'numeric', month: 'short' })
+                                    ),
+                                    datasets: [{
+                                        label: '支出額',
+                                        data: monthlyExpensesList.map(item => item.total_amount),
+                                        backgroundColor: '#FF6384',
+                                        borderColor: '#FF6384',
+                                        // borderWidth: 1
+                                    }]
+                                }}
+                            />
+
                             {fetchItem.data.map((item) => (
                                 <div key={item.id} onClick={() => navigate(`/item-registry/${item.id}`)} className="flex items-center justify-between p-2 border-b">
                                     <div className="flex items-center">
